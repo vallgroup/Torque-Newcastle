9ffbb570486789e8be4af61244073d3227a32756
Mario Vallejo <mario@vallgroup.com>
2021-05-20 22:12:40 -0500

    remove console logs

--- src/parts/acf/modules/anchor-section.php
+++ src/parts/acf/modules/anchor-section.php
@@ -9,10 +9,8 @@
   (function($){
     var offset = Number(<?php echo $offset; ?>)
     var a = $('a[href="#<?php echo $anchor_id; ?>"]')
-    console.log({a, offset});
     a.on('click', function(e) {
       e.preventDefault();
-      console.log('hi');
       var el = $("#<?php echo $anchor_id; ?>");
       window.scrollTo({
         top: el.offset().top - offset,
