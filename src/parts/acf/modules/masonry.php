c083de13a5c40849ca4020fe76a559d32ae30ca5
Jorge Cortez <jorge@vallgroup.com>
2023-01-17 19:12:08 -0400

    PHOT-85 Update newcastle gallery

--- src/parts/acf/modules/masonry.php
+++ src/parts/acf/modules/masonry.php
@@ -11,14 +11,14 @@
 
   <div class="masonry-gallery">
     <?php if ( $images ) {
-      $wide_values = [1, 4, 7, 10, 13, 16];
-      $half_values = [5, 6, 11, 12];
+      $quarter_values = [1, 3, 7, 9, 13, 15, 19, 21];
+      $half_values = [2, 8, 14, 20];
 
       $index = 0;
       foreach ( $images as $image ) {
         $index++;
         ?>
-        <div class="masonry-item <?php if( in_array( $index, $wide_values ) ) { echo 'wide'; } if( in_array( $index, $half_values ) ) { echo 'half'; } ?>">
+        <div class="masonry-item <?php if( in_array( $index, $quarter_values ) ) { echo 'quarter'; } if( in_array( $index, $half_values ) ) { echo 'half'; } ?>">
           <img src="<?php echo $image['url'] ?>" />
         </div>
     <?php }
